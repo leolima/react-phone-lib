d9fced777c9c97f73e4a9e4bb26af9e9c95b243e
Leo <leolima.str@gmail.com>
2020-03-11 13:41:17 -0300

    feat: incrementing the demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,32 +1,59 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import PhoneInput from '../../dist/main';
+import PhoneInput from "react-phone-lib";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      phone: '',
-    }
+      phone: ""
+    };
   }
 
-  handleChangeNumber = (val) => {
+  handleChangeNumber = val => {
     this.setState({ phone: val });
-  }
+  };
+
+  handleOnBlur = () => {
+    console.log("onBlur called");
+  };
+
+  handleOnFocus = () => {
+    console.log("onFocus called");
+  };
+
+  validateNumber = (obj) => {
+    if (obj && obj.isValid) {
+      console.log("The number is valid");
+    }
+  };
 
   render() {
     const { phone } = this.state;
     return (
       <div>
         <h2>React Phone lib - Demo</h2>
-        <PhoneInput debug value={phone} onChange={this.handleChangeNumber} />
+        <PhoneInput
+          debug
+          autoFormat
+          value={phone}
+          onChange={this.handleChangeNumber}
+          onBlur={this.handleOnBlur}
+          callBack={this.validateNumber}
+          onFocus={this.handleOnFocus}
+          format="INTERNATIONAL"
+          language="es"
+          label="Phone"
+          inputId="phone"
+          defaultCountry="ES"
+          inputClass="input-class"
+          selectClass="select-class"
+          containerClass="container-class"
+        />
       </div>
     );
   }
 }
 
-render(
-  <App />, 
-  document.getElementById('app')
-);
+render(<App />, document.getElementById("app"));
